374bf08556d904501f49b7e4c723bd7f5f690a40
Fame <fame5454@gmail.com>
2023-10-20 14:24:39 +0700

    Edit student detail view

--- src/views/details/StudentDetailView.vue
+++ src/views/details/StudentDetailView.vue
@@ -2,36 +2,52 @@
 import CommentForm from '@/components/CommentForm.vue'
 import CommentList from '@/components/CommentList.vue'
 import type { StudentItem } from '@/type'
-import { type PropType } from 'vue'
+import { defineProps, type PropType } from 'vue'
+
 defineProps({
   student: {
     type: Object as PropType<StudentItem>,
-    require: true
+    required: true
   }
 })
 </script>
 
 <template>
   <div v-if="student" class="flex justify-center items-center">
-    <div class="max-w-2xl mx-auto p-4 bg-gray-100 rounded-lg shadow-md">
-      <div class="text-center">
-        <h2 class="text-2xl font-semibold mb-2">{{ student?.name }} {{ student?.surname }}</h2>
-        <p class="text-gray-600"><span class="font-semibold">ID:</span> {{ student?.id }}</p>
-        <p class="text-gray-600"><span class="font-semibold">Enrolled Courses: </span> 
-          <ul>
-            <li v-for="courseId in student?.courseId" :key="courseId">{{ courseId }}</li>
-          </ul>
-        </p>
-        <img :src="student?.image" alt="" class="mt-4 rounded-lg shadow-md max-w-full h-auto">
+    <div class="max-w-3xl mx-auto p-6 bg-gray-100 rounded-lg shadow-md">
+      <div class="flex">
+        <div class="w-1/3">
+          <img :src="student.image" alt="Student Image" class="rounded-lg shadow-md max-w-full h-auto" />
+        </div>
+        <div class="w-2/3 p-6">
+          <h2 class="text-3xl font-semibold mb-4">{{ student.name }} {{ student.surname }}</h2>
+          <p class="text-lg text-gray-600 mb-2">
+            <span class="font-semibold">ID:</span> {{ student.id }}
+          </p>
+          <div class="text-lg text-gray-600 mb-4">
+            <span class="font-semibold">Enrolled Courses:</span>
+            <ul class="list-disc list-inside">
+              <li v-for="courseId in student.courseId" :key="courseId">{{ courseId }}</li>
+            </ul>
+          </div>
+        </div>
       </div>
-      <CommentForm class="mt-4" :studentId="student?.id"></CommentForm>
-      <CommentList class="mt-4" :studentId="student?.id" :key="student?.id"></CommentList>
+      <CommentForm class="mt-4" :studentId="student.id" />
+      <CommentList class="mt-4" :studentId="student.id" :key="student.id" />
     </div>
   </div>
 </template>
 
-<style>
+<style scoped>
+.max-w-3xl {
+  max-width: 48rem;
+}
 
+.text-3xl {
+  font-size: 1.875rem;
+}
+
+.text-lg {
+  font-size: 1.125rem;
+}
 </style>
-
-
