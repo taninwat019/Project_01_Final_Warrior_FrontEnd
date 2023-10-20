85b0123967c1ecf53efe969ca7276d2781b6dcad
Fame <fame5454@gmail.com>
2023-10-21 01:04:25 +0700

    Edit advisor and student detail page

--- src/views/details/AdvisorDetailView.vue
+++ src/views/details/AdvisorDetailView.vue
@@ -1,47 +1,49 @@
 <script setup lang="ts">
-import { type Advisor } from '@/type'
-import type { PropType } from 'vue'
+import { defineProps } from 'vue';
+import { Advisor } from '@/type';
+import Announcement from '@/components/Anouncement.vue'
+
 
 defineProps({
   advisor: {
-    type: Object as PropType<Advisor>,
+    type: Object as () => Advisor,
     required: true
   }
-})
+});
 </script>
 
 <template>
   <div class="max-w-md mx-auto p-4 bg-gray-100 rounded-lg shadow-md">
     <div class="text-center">
       <div class="mb-4">
-        <img :src="advisor?.image" alt="advisor image" class="rounded-lg shadow-md custom-image"> <!-- เพิ่ม custom-image ที่นี่ -->
+        <img :src="advisor?.image" alt="advisor image" class="rounded-lg shadow-md custom-image">
       </div>
     </div>
-  </div>
-  <div class="text-center">
-    <div class="mb-6 mt-4"> 
-      <label for="firstName" class="block text-gray-700 text-xl font-bold mb-2">First Name:</label>
-      <h2 class="text-base font-semibold mb-2">{{ advisor?.name }}</h2>
-    </div>
-    <div class="mb-6"> 
-      <label for="lastName" class="block text-gray-700 text-xl font-bold mb-2">Last Name:</label>
-      <h2 class="text-base font-semibold mb-2">{{ advisor?.surname }}</h2>
-    </div>
-    <div class="mb-6"> 
-      <label for="lastName" class="block text-gray-700 text-xl font-bold mb-2">Department:</label>
-      <h2 class="text-base font-semibold mb-2">{{ advisor?.surname }}</h2>
-    </div>
-    <div class="mb-6"> 
-      <label for="lastName" class="block text-gray-700 text-xl font-bold mb-2">Position:</label>
-      <h2 class="text-base font-semibold mb-2">{{ advisor?.surname }}</h2>
-    </div>
-    <div class="flex justify-center"> 
-      <router-link to="/advisor" class="text-blue-500 inline-block mx-4">
-        <button @click="" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Back</button>
-      </router-link>
-      <router-link to="/advisee" class="text-blue-500 inline-block">
-        <button @click="" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Advisee</button>
-      </router-link>
+    <div class="text-center">
+      <div class="mb-6 mt-4">
+        <label for="firstName" class="block text-gray-700 text-xl font-bold mb-2">First Name:</label>
+        <h2 class="text-base font-semibold mb-2">{{ advisor?.name }}</h2>
+      </div>
+      <div class="mb-6">
+        <label for="lastName" class="block text-gray-700 text-xl font-bold mb-2">Last Name:</label>
+        <h2 class="text-base font-semibold mb-2">{{ advisor?.surname }}</h2>
+      </div>
+      <div class="mb-6">
+        <label for="department" class="block text-gray-700 text-xl font-bold mb-2">Department:</label>
+        <h2 class="text-base font-semibold mb-2">{{ advisor?.department }}</h2>
+      </div>
+      <div class="mb-6">
+        <label for="position" class="block text-gray-700 text-xl font-bold mb-2">Position:</label>
+        <h2 class="text-base font-semibold mb-2">{{ advisor?.position }}</h2>
+      </div>
+      <div class="flex justify-center mt-6">
+        <router-link to="/advisor" class="text-blue-500 inline-block mx-4">
+          <button @click="" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Back</button>
+        </router-link>
+        <router-link to="/advisee" class="text-blue-500 inline-block">
+          <button @click="" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Advisee</button>
+        </router-link>
+      </div>
     </div>
   </div>
 </template>
@@ -49,10 +51,13 @@ defineProps({
 <style>
 .max-w-md {
   max-width: 30rem;
+  margin: auto;
 }
 
 .custom-image {
-  max-width: 100%;
-  height: auto;
+  width: 100%;
+  max-height: 300px;
+  object-fit: cover;
+  border-radius: 0.5rem;
 }
 </style>
--- src/views/details/StudentDetailView.vue
+++ src/views/details/StudentDetailView.vue
@@ -1,36 +1,22 @@
-<script setup lang="ts">
-import CommentForm from '@/components/CommentForm.vue'
-import CommentList from '@/components/CommentList.vue'
-import type { StudentItem } from '@/type'
-import { defineProps, type PropType } from 'vue'
-
-defineProps({
-  student: {
-    type: Object as PropType<StudentItem>,
-    required: true
-  }
-})
-</script>
-
 <template>
   <div v-if="student" class="flex justify-center items-center">
-    <div class="max-w-3xl mx-auto p-6 bg-gray-100 rounded-lg shadow-md">
-      <div class="flex">
-        <div class="w-1/3">
-          <img :src="student.image" alt="Student Image" class="rounded-lg shadow-md max-w-full h-auto" />
+    <div class="max-w-3xl mx-auto p-6 bg-white rounded-lg shadow-md my-4">
+      <div class="grid md:grid-cols-2 gap-4">
+        <div class="w-full h-72 md:h-96 lg:h-80">
+          <img :src="student?.image" :alt="student?.name" class="w-full h-full object-cover rounded-lg" />
         </div>
-        <div class="w-2/3 p-6">
+        <div class="w-full p-6">
           <h2 class="text-3xl font-semibold mb-4">{{ student.name }} {{ student.surname }}</h2>
           <p class="text-lg text-gray-600 mb-2">
             <span class="font-semibold">ID:</span> {{ student.id }}
           </p>
           <p class="text-lg text-gray-600 mb-2">
-            <span class="font-semibold">Department:</span> {{ student.id }}
+            <span class="font-semibold">Department:</span> {{ student.department }}
           </p>
           <div class="text-lg text-gray-600 mb-4">
             <span class="font-semibold">Enrolled Courses:</span>
             <ul class="list-disc list-inside">
-              <li v-for="courseId in student.courseId" :key="courseId">{{ courseId }}</li>
+              <li v-for="courseId in student.courseId" :key="courseId" class="mb-2">{{ courseId }}</li>
             </ul>
           </div>
         </div>
@@ -40,6 +26,20 @@ defineProps({
     </div>
   </div>
 </template>
+
+<script setup lang="ts">
+import CommentForm from '@/components/CommentForm.vue';
+import CommentList from '@/components/CommentList.vue';
+import type { StudentItem } from '@/type';
+import { defineProps, type PropType } from 'vue';
+
+defineProps({
+  student: {
+    type: Object as PropType<StudentItem>,
+    required: true
+  }
+});
+</script>
 
 <style scoped>
 .max-w-3xl {
